35d5f9019ca234771711c5697db7e378ca8a00e0
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2020-10-03 11:33:44 -0300

    Inicializador de proyecto

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="">Tailwind</h1>
+            <h2>Css</h2>
+            </div>
+    )
+}
+
+export default App;
