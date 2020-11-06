b67a4a48fb9396cbc7ec347f01f55d81b88dd580
Jacques Frank <jacqueshuberson@gmail.com>
2020-11-06 01:53:26 -0300

    servicios
    cambios minimos

---
+++ src/services/techicalSService.js
@@ -0,0 +1,24 @@
+import http from '../helpers/http-common';
+
+class TechnicalServiceService {
+    getAll() {
+      return http.get("/TechnicalService");
+    }
+
+    getById(id) {
+      return http.get(`/TechnicalService/${id}`);
+    }
+
+    create(data) {
+      return http.post("/TechnicalService", data);
+    }
+  
+    update(id, data) {
+      return http.put(`/TechnicalService/${id}`, data);
+    }
+  
+    delete(id) {
+      return http.delete(`/TechnicalService/${id}`);
+    }
+}
+export default new TechnicalServiceService();
