3a3d9a70402d8655531f5824f5871f00947b27e8
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2020-11-21 21:39:14 -0300

    Updates

---
+++ precache-manifest.8416ba0631f39660f460ed22fed38b3d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f4f4a889419f94b41d6c3e6b7db85e",
+    "url": "/MundoPc/index.html"
+  },
+  {
+    "revision": "3ef992b6b89785373ea7",
+    "url": "/MundoPc/static/css/main.4227f05b.chunk.css"
+  },
+  {
+    "revision": "b52db1640a6cd98efbff",
+    "url": "/MundoPc/static/js/2.7fe89f13.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/MundoPc/static/js/2.7fe89f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae35187d13e6a821ee3",
+    "url": "/MundoPc/static/js/3.d826c4a3.chunk.js"
+  },
+  {
+    "revision": "8300958b8f0020bbae45d0240e91020f",
+    "url": "/MundoPc/static/js/3.d826c4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485cb4b6bae7ee29b3a6",
+    "url": "/MundoPc/static/js/4.b21cbe45.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/MundoPc/static/js/4.b21cbe45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef992b6b89785373ea7",
+    "url": "/MundoPc/static/js/main.377185ee.chunk.js"
+  },
+  {
+    "revision": "58705bab57236971bfa4",
+    "url": "/MundoPc/static/js/runtime-main.3c9c9aaf.js"
+  },
+  {
+    "revision": "38b82a9e94a868ec81c489dc97f334ac",
+    "url": "/MundoPc/static/media/Consulta.38b82a9e.jpg"
+  },
+  {
+    "revision": "d840a0313e58d8a8c038a95de1e37c95",
+    "url": "/MundoPc/static/media/Login.d840a031.jpeg"
+  },
+  {
+    "revision": "4643c5cda35e4c657f2b288163d840a6",
+    "url": "/MundoPc/static/media/LogoFooter.4643c5cd.jpg"
+  },
+  {
+    "revision": "d2a38e069736323ca592eb4a5e657528",
+    "url": "/MundoPc/static/media/RecuperarCuenta.d2a38e06.jpg"
+  },
+  {
+    "revision": "afa928ad078b09223338843502e16ccd",
+    "url": "/MundoPc/static/media/Registro.afa928ad.jpg"
+  },
+  {
+    "revision": "cbc850ff307a55f85c3bfbdcbb87b32f",
+    "url": "/MundoPc/static/media/ServicioTecnico.cbc850ff.jpg"
+  },
+  {
+    "revision": "e816501587aee6a5d5e375377ca3b815",
+    "url": "/MundoPc/static/media/bannerHome.e8165015.jpg"
+  }
+]);
